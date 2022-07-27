58b302adebd5c0b8d05a6f66d6724e00ff7de0ef
Alex Remache <80859829+alexrch97@users.noreply.github.com>
2022-07-27 01:31:42 -0500

    Create get_info_lb1.py

---
+++ get_info_lb1.py
@@ -0,0 +1,18 @@
+# Programa que recolecta la informacion de la interfaz
+# LoopBack1 del agente
+# Autor: Alex Remache
+# Fecha: Julio 14, 2022
+# Version: 1.0
+# Metodo HTTPS: GET
+
+# Librerias
+import sys # acceso a variables
+sys.path.append("/home/devasc/labs/devnet-src/restconf_labs") # directorio actual
+import info_basica as i # info_basica.py
+import metodo_get as get # metodo_get.py
+
+# URL
+url = f"https://{i.server}:{i.port}/{i.rest_path}/data/ietf-interfaces:interfaces/interface=Loopback1" 
+print(url)
+
+get.metodo_get(url)
