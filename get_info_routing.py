7f2e103fd6ec33ff1d075fc339e48cb8c7219484
Alex Remache <80859829+alexrch97@users.noreply.github.com>
2022-07-27 01:41:49 -0500

    Create get_info_routing.py

---
+++ get_info_routing.py
@@ -0,0 +1,17 @@
+# Programa que recolecta la informacion routing del servidor
+# Autor: Alex Remache
+# Fecha: Julio 14, 2022
+# Version: 1.0
+# Metodo HTTPS: GET
+
+# librerias
+import sys # acceso a variables
+sys.path.append("/home/devasc/labs/devnet-src/restconf_labs") # directorio actual
+import info_basica as i # info_basica.py
+import metodo_get as get # metodo_get.py
+
+# URL
+url = f"https://{i.server}:{i.port}/{i.rest_path}/data/ietf-routing:routing-state" 
+print(url)
+
+get.metodo_get(url)
